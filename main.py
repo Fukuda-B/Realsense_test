637c3b7cd1811742230f326eb2f7c3a271fe1573
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2022-04-06 18:51:06 +0900

    update main.py

--- main.py
+++ main.py
@@ -21,73 +21,76 @@ class Settings():
         self.F_NAME = 'realsense_b.bag'
         self.FULL_NAME = os.path.join(desktop, self.F_NAME)
 
-def rec(settings):
-    config = rs.config()
-    config.enable_stream(rs.stream.infrared, 1, *settings.V_SIZE, rs.format.y8, settings.FPS)
-    config.enable_stream(rs.stream.depth, *settings.V_SIZE, rs.format.z16, settings.FPS)
-    config.enable_stream(rs.stream.color, *settings.V_SIZE, rs.format.bgr8, settings.FPS)
-    config.enable_record_to_file(settings.FULL_NAME)
+class Realsense_test():
+    def rec(settings):
+        config = rs.config()
+        config.enable_stream(rs.stream.infrared, 1, *settings.V_SIZE, rs.format.y8, settings.FPS)
+        config.enable_stream(rs.stream.depth, *settings.V_SIZE, rs.format.z16, settings.FPS)
+        config.enable_stream(rs.stream.color, *settings.V_SIZE, rs.format.bgr8, settings.FPS)
+        config.enable_record_to_file(settings.FULL_NAME)
 
-    pipeline = rs.pipeline()
-    pipeline.start(config)
-    start = time.time()
-    frame_no = 1
-    try:
-        while True:
-            frames = pipeline.wait_for_frames()
-            color_frame = frames.get_color_frame()
-            ir_frame = frames.get_infrared_frame()
-            fps  = frame_no / (time.time() - start)  
-            print(fps)
-            frame_no = frame_no+1  
-            if not ir_frame or not color_frame  :   
-                ir_image = np.asanyarray(ir_frame .get_data())    
-                color_image = np.asanyarray(color_frame.get_data()) 
-    except Exception as e:
-        print(e)
-    finally:    
-        pipeline.stop()
+        pipeline = rs.pipeline()
+        pipeline.start(config)
+        start = time.time()
+        frame_no = 1
+        try:
+            while True:
+                frames = pipeline.wait_for_frames()
+                color_frame = frames.get_color_frame()
+                ir_frame = frames.get_infrared_frame()
+                fps  = frame_no / (time.time() - start)
+                print(fps)
+                frame_no += 1
+                if not ir_frame or not color_frame:
+                    ir_image = np.asanyarray(ir_frame.get_data())
+                    color_image = np.asanyarray(color_frame.get_data())
+        except Exception as e:
+            print(e)
+        finally:    
+            pipeline.stop()
 
-def play(settings):
-    config = rs.config()
-    config.enable_device_from_file(settings.FULL_NAME)
-    config.enable_stream(rs.stream.color, *settings.V_SIZE, rs.format.bgr8, settings.FPS)
-    config.enable_stream(rs.stream.depth, *settings.V_SIZE, rs.format.z16, settings.FPS)
-    config.enable_stream(rs.stream.infrared, 1, *settings.V_SIZE, rs.format.y8, settings.FPS)
+    def play(settings):
+        config = rs.config()
+        config.enable_device_from_file(settings.FULL_NAME)
+        config.enable_stream(rs.stream.color, *settings.V_SIZE, rs.format.bgr8, settings.FPS)
+        config.enable_stream(rs.stream.depth, *settings.V_SIZE, rs.format.z16, settings.FPS)
+        config.enable_stream(rs.stream.infrared, 1, *settings.V_SIZE, rs.format.y8, settings.FPS)
 
-    pipeline = rs.pipeline()
-    profile = pipeline.start(config)
+        pipeline = rs.pipeline()
+        profile = pipeline.start(config)
 
-    try:
-        while True:
-            frames = pipeline.wait_for_frames()
-            ir_frame = frames.get_infrared_frame()
-            depth_frame = frames.get_depth_frame()
-            color_frame = frames.get_color_frame()
-            if not depth_frame or not color_frame or not ir_frame  :
-                continue
+        try:
+            while True:
+                frames = pipeline.wait_for_frames()
+                ir_frame = frames.get_infrared_frame()
+                depth_frame = frames.get_depth_frame()
+                color_frame = frames.get_color_frame()
+                if not depth_frame or not color_frame or not ir_frame:
+                    continue
 
-            ir_image = np.asanyarray(ir_frame .get_data())
-            depth_image = np.asanyarray(depth_frame.get_data())
-            color_image = np.asanyarray(color_frame.get_data())       
+                ir_image = np.asanyarray(ir_frame .get_data())
+                depth_image = np.asanyarray(depth_frame.get_data())
+                color_image = np.asanyarray(color_frame.get_data())
 
-            cv2.namedWindow('ir_image', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('ir_image', ir_image)
-            cv2.namedWindow('color_image', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('color_image', color_image)
-            cv2.namedWindow('depth_image', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('depth_image', depth_image)
-            cv2.waitKey(1)
-    except Exception as e:
-        print(e)
-    finally:
-        pipeline.stop()
+                cv2.namedWindow('ir_image', cv2.WINDOW_AUTOSIZE)
+                cv2.imshow('ir_image', ir_image)
+                cv2.namedWindow('color_image', cv2.WINDOW_AUTOSIZE)
+                cv2.imshow('color_image', color_image)
+                cv2.namedWindow('depth_image', cv2.WINDOW_AUTOSIZE)
+                cv2.imshow('depth_image', depth_image)
+                cv2.waitKey(1)
+        except Exception as e:
+            print(e)
+        finally:
+            pipeline.stop()
 
 if __name__ == "__main__":
     settings = Settings()
-    option = sys.argv[1]
-
-    if str(option) == 'rec':
-        rec(settings)
+    tester = Realsense_test()
+    if len(sys.argv) >= 2:
+        if str(sys.argv[1]) == 'rec':
+            tester.rec(settings)
+        else:
+            tester.play(settings)
     else:
-        play(settings)
+        print(f'----- option -----\nrecode: py main.py rec\nplay  : py main.py play')
